9c7b8c7679faef8f290514f0112d40510b4da069
Shrey Chandra <a1788528@student.adelaide.edu.au>
2021-09-23 02:24:09 +0930

    5 day forecast added to cards

--- js/index.js
+++ js/index.js
@@ -1,27 +1,96 @@
 var searchButton = document.querySelector("#searchButtonId");
-let currentCity;
+let currentCity, currentCityLat, currentCityLong, countryName;
 
 let currentCityAndDateTitle = document.querySelector("#cityCurrentAndDate");
 let currentCityTemp = document.querySelector(".temp");
+let currentCityWind = document.querySelector(".wind");
+let currentCityHumidity = document.querySelector(".humidity");
+let currentCityUv = document.querySelector(".uv");
+
+let cardElement1 = document.querySelector(".card1");
 
 const apiKey = "47f166773e351368285402b79068ea73";
 
-let api = "";
+let cityCoordinatesApi = "";
+let weatherApi = "";
+
+function setIcons(icon, iconIDsky) {
+  if (icon == "SNOW") var skycons = new Skycons({ color: "black" });
+  else var skycons = new Skycons({ color: "black" });
+  skycons.play();
+  return skycons.set(iconIDsky, Skycons[icon]);
+}
+
+function covertToHumanDate(unixTimestamp) {
+  const milliseconds = unixTimestamp * 1000; // 1575909015000
+  const dateObject = new Date(milliseconds);
+  const humanDateFormat = dateObject.toLocaleString(); //2019-12-9 10:30:15
+  const humanDate = humanDateFormat.split(",")[0];
+  return humanDate;
+}
 
 searchButton.addEventListener("click", () => {
   console.log(document.querySelector(".cityNameInput").value);
   currentCity = document.querySelector(".cityNameInput").value;
-  api = `http://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&units=metric&appid=${apiKey}`;
-  console.log(api);
-  fetch(api)
+  cityCoordinatesApi = `http://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&limit=1&appid=${apiKey}`;
+  // getting the geo coordinates based on the city name
+  fetch(cityCoordinatesApi)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      console.log(data.city.name);
-      console.log(data.city.country);
-      console.log(data.list[0].dt_txt);
-      console.log(data.list[0].main.temp);
-      currentCityAndDateTitle.innerText = `${data.city.name}, ${data.city.country} `;
-      currentCityTemp.innerText = `Temp: ${data.list[0].main.temp}`;
+      currentCityLat = data[0].lat;
+      currentCityLong = data[0].lon;
+      countryName = data[0].country;
+      currentCity = data[0].name;
+      console.log("------_XXXX-------");
+      console.log(currentCityLat, currentCityLong, countryName, currentCity);
+
+      weatherApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentCityLat}&lon=${currentCityLong}&units=metric&exclude=minutely,hourly&appid=${apiKey}`;
+      fetch(weatherApi)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          //get human readable Datetime
+          let unixTimestamp = data.current.dt;
+          let humanDate = covertToHumanDate(unixTimestamp);
+          // console.log(humanDateFormat, "XXXX", humanDate);
+
+          currentCityAndDateTitle.innerText = `${currentCity}, ${countryName} (${humanDate})`;
+          currentCityTemp.innerText = `Temp: ${data.current.temp} °C`;
+          currentCityWind.innerText = `Wind: ${Math.round(
+            data.current.wind_speed * 3.6
+          )} km/h`;
+          currentCityHumidity.innerText = `Humidity: ${data.current.humidity} %`;
+          currentCityUv.innerText = `UV Index: ${data.current.uvi}`;
+          var icon = "CLEAR_DAY";
+          setIcons(icon, document.querySelector(".icon"));
+
+          for (var i = 1; i <= 5; i++) {
+            let cardDate = covertToHumanDate(data.daily[i].dt);
+            const cardDateElement = document.createElement("p");
+            cardDateElement.innerHTML = cardDate;
+            let card = `.card` + i;
+            let cardElement = document.querySelector(`${card}`);
+            cardElement.appendChild(cardDateElement);
+
+            let cardTemp = data.daily[i].temp.day;
+            const cardTempElement = document.createElement("p");
+            cardTempElement.innerHTML = `Temp: ${cardTemp}`;
+            cardElement.appendChild(cardTempElement);
+
+            let cardWind = Math.round(data.daily[i].wind_speed * 3.6);
+            const cardWindElement = document.createElement("p");
+            cardWindElement.innerHTML = `Wind: ${cardWind} km/h`;
+            cardElement.appendChild(cardWindElement);
+
+            let cardHumidity = data.daily[i].humidity;
+            const cardHumidityElement = document.createElement("p");
+            cardHumidityElement.innerHTML = `Humidity: ${cardHumidity}`;
+            cardElement.appendChild(cardHumidityElement);
+
+            console.log(`-------[${i}]---------`);
+            console.log(cardDate, cardTemp, cardWind, cardHumidity);
+          }
+        });
     });
 });
