eae856cedbc74786f0d65b38cd71dff56d04ef3c
Shrey Chandra <a1788528@student.adelaide.edu.au>
2021-09-26 19:28:50 +0930

    Refactoring, default city load, button flickerking handled

--- js/index.js
+++ js/index.js
@@ -1,51 +1,31 @@
-let searchButton = document.querySelector("#searchButtonId");
-let currentCity, currentCityLat, currentCityLong, countryName;
-let currentCityAndDateTitle = document.querySelector("#cityCurrentAndDate");
-let currentCityTemp = document.querySelector(".temp");
-let currentCityWind = document.querySelector(".wind");
-let currentCityHumidity = document.querySelector(".humidity");
-let currentCityUv = document.querySelector(".uv");
-let cardElement1 = document.querySelector(".card1");
+const searchButton = document.querySelector("#searchButtonId"),
+  currentCityAndDateTitle = document.querySelector("#cityCurrentAndDate"),
+  currentCityTemp = document.querySelector(".temp"),
+  currentCityWind = document.querySelector(".wind"),
+  currentCityHumidity = document.querySelector(".humidity"),
+  currentCityUv = document.querySelector(".uv"),
+  cardElement1 = document.querySelector(".card1"),
+  allButtons = document.getElementById("cityList"),
+  searchBox = document.querySelector(".cityNameInput"),
+  cards = document.getElementsByClassName("card"),
+  spinner = document.getElementById("spinner");
 
-const apiKey = "47f166773e351368285402b79068ea73";
-
-let cityCoordinatesApi = "";
-let weatherApi = "";
-let cardDateElement = "";
-let cardIconElement = "";
-let cardTempElement = "";
-let cardWindElement = "";
-let cardHumidityElement = "";
-let allCityNamesSplit = [];
-let allButtons = document.getElementById("cityList");
-
-window.onload = () => {
-  let previousCityNames = localStorage.getItem("cityName") || "";
-  if (previousCityNames.length) {
-    previousCityNames = previousCityNames.split(",");
-    for (let i = 0; i < previousCityNames.length; i++) {
-      let cityButton = document.createElement("button");
-      cityButton.className = "cityButtons buttons";
-      cityButton.innerText = previousCityNames[i];
-      cityButton.setAttribute("value", previousCityNames[i]);
-      allButtons.appendChild(cityButton);
-    }
-  }
-};
-
-const spinner = document.getElementById("spinner");
+const apiKey = "47f166773e351368285402b79068ea73",
+  localStorageKey = "cityName";
 
 const cityCall = async (city) => {
-  allButtons.innerHTML = "";
-  // allButtons.style.visibility = "hidden";
   spinner.removeAttribute("hidden");
   try {
-    cityCoordinatesApi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+    const cityCoordinatesApi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
     // getting the geo coordinates based on the city name
     const response = await fetch(cityCoordinatesApi);
     const data = await response.json();
-    const cityData = weatherApiCall(data);
-    return cityData;
+    const { weatherApi, countryName, currentCity } = weatherApiCall(data);
+    return {
+      weatherApi,
+      countryName,
+      currentCity,
+    };
   } catch (err) {
     alert("Oops! are you sure that's a city ?");
     window.location.reload();
@@ -57,23 +37,6 @@ const weatherUpdate = async (LatLongData) => {
   const weatherData = await responseWeather.json();
   return weatherData;
 };
-
-const controllerFunc = async (evt) => {
-  let city = "";
-  if (evt.target.value != "") {
-    city = evt.target.value;
-  } else {
-    city = document.querySelector(".cityNameInput").value;
-  }
-  const LatLongData = await cityCall(city);
-  const weatherUpdates = await weatherUpdate(LatLongData);
-
-  showWeather(weatherUpdates);
-};
-
-searchButton.addEventListener("click", controllerFunc);
-
-allButtons.addEventListener("click", controllerFunc);
 
 const setIcons = (icon, iconIDsky) => {
   let skycons = new Skycons({ color: "white" });
@@ -113,44 +76,23 @@ const createSearchButton = (currentCityName) => {
 };
 
 const weatherApiCall = (data) => {
-  document.querySelector(".cityNameInput").value = "";
-  currentCityLat = data[0].lat;
-  currentCityLong = data[0].lon;
-  countryName = data[0].country;
-  currentCity = data[0].name;
+  const currentCityLat = data[0].lat,
+    currentCityLong = data[0].lon,
+    countryName = data[0].country,
+    currentCity = data[0].name;
 
-  let previousCityNames = localStorage.getItem("cityName") || "";
-  if (previousCityNames.length === 0) {
-    allCityNamesSplit = [currentCity];
-  } else {
-    allCityNamesSplit = previousCityNames.split(",");
-  }
-  if (!allCityNamesSplit.includes(currentCity)) {
-    if (allCityNamesSplit.length > 5) {
-      allCityNamesSplit.shift();
-      allCityNamesSplit.push(currentCity);
-    } else {
-      allCityNamesSplit.push(currentCity);
-    }
-  }
+  let weatherApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentCityLat}&lon=${currentCityLong}&units=metric&exclude=minutely,hourly&appid=${apiKey}`;
 
-  if (previousCityNames.length === 0) {
-    createSearchButton(currentCity);
-  } else {
-    for (let i = 0; i < allCityNamesSplit.length; i++) {
-      createSearchButton(allCityNamesSplit[i]);
-    }
-  }
-
-  localStorage.setItem("cityName", allCityNamesSplit.join());
-
-  weatherApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentCityLat}&lon=${currentCityLong}&units=metric&exclude=minutely,hourly&appid=${apiKey}`;
-
-  return weatherApi;
+  return {
+    weatherApi,
+    countryName,
+    currentCity,
+  };
 };
 
-const showWeather = (data) => {
+const showWeather = (data, countryName, currentCity) => {
   spinner.setAttribute("hidden", "");
+  searchBox.value = "";
   let unixTimestamp = data.current.dt;
   let humanDate = covertToHumanDate(unixTimestamp);
 
@@ -172,40 +114,85 @@ const showWeather = (data) => {
   let icon = convertIcons(data.current.weather[0].main);
   setIcons(icon, document.querySelector(".icon"));
 
-  let cards = document.getElementsByClassName("card");
   for (let j = 0; j <= 4; j++) {
     cards[j].innerHTML = "";
   }
 
   for (let i = 1; i <= 5; i++) {
-    let cardDate = covertToHumanDate(data.daily[i].dt);
-    cardDateElement = document.createElement("p");
+    let cardDate = covertToHumanDate(data.daily[i].dt),
+      cardDateElement = document.createElement("p");
     cardDateElement.innerHTML = cardDate;
     let card = `.card` + i;
     let cardElement = document.querySelector(`${card}`);
     cardElement.appendChild(cardDateElement);
 
-    let cardIcon = convertIcons(data.daily[i].weather[0].main);
-    cardIconElement = document.createElement("canvas");
+    let cardIcon = convertIcons(data.daily[i].weather[0].main),
+      cardIconElement = document.createElement("canvas");
     cardIconElement.width = "30";
     cardIconElement.height = "30";
     cardIconElement.className = "cardIcon" + i;
     cardElement.appendChild(cardIconElement);
     setIcons(cardIcon, document.querySelector(`.cardIcon${i}`));
 
-    let cardTemp = data.daily[i].temp.day;
-    cardTempElement = document.createElement("p");
+    let cardTemp = data.daily[i].temp.day,
+      cardTempElement = document.createElement("p");
     cardTempElement.innerHTML = `Temp: ${cardTemp}`;
     cardElement.appendChild(cardTempElement);
 
-    let cardWind = Math.round(data.daily[i].wind_speed * 3.6);
-    cardWindElement = document.createElement("p");
+    let cardWind = Math.round(data.daily[i].wind_speed * 3.6),
+      cardWindElement = document.createElement("p");
     cardWindElement.innerHTML = `Wind: ${cardWind} km/h`;
     cardElement.appendChild(cardWindElement);
 
-    let cardHumidity = data.daily[i].humidity;
-    cardHumidityElement = document.createElement("p");
+    let cardHumidity = data.daily[i].humidity,
+      cardHumidityElement = document.createElement("p");
     cardHumidityElement.innerHTML = `Humidity: ${cardHumidity}`;
     cardElement.appendChild(cardHumidityElement);
+  }
+};
+
+const updateHistory = (currentCity) => {
+  let cities = localStorage.getItem(localStorageKey) || "";
+  if (!cities.length) {
+    cities += currentCity;
+    createSearchButton(currentCity);
+  } else {
+    if (!cities.includes(currentCity)) {
+      cities += `,${currentCity}`;
+      createSearchButton(currentCity);
+    }
+  }
+  localStorage.setItem(localStorageKey, cities);
+};
+
+const onClickHandler = async (evt) => {
+  let city = "";
+  if (evt.target.value != "") {
+    city = evt.target.value;
+  } else {
+    city = document.querySelector(".cityNameInput").value;
+  }
+  const { weatherApi, countryName, currentCity } = await cityCall(city);
+  const weatherUpdates = await weatherUpdate(weatherApi);
+  updateHistory(currentCity);
+  showWeather(weatherUpdates, countryName, currentCity);
+};
+
+searchButton.addEventListener("click", onClickHandler);
+
+allButtons.addEventListener("click", onClickHandler);
+
+window.onload = async () => {
+  let previousCityNames = localStorage.getItem(localStorageKey) || "";
+  if (previousCityNames.length) {
+    previousCityNames = previousCityNames.split(",");
+    for (let i = 0; i < previousCityNames.length; i++) {
+      createSearchButton(previousCityNames[i]);
+    }
+    const { weatherApi, countryName, currentCity } = await cityCall(
+      previousCityNames[0]
+    );
+    const weatherUpdates = await weatherUpdate(weatherApi);
+    showWeather(weatherUpdates, countryName, currentCity);
   }
 };
